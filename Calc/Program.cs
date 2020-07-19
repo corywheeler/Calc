61f19b3112161d0413db963b573e07e61d1aee45
Cory Wheeler <corywheeler@gmail.com>
2020-07-18 21:09:36 -0400

    Initialize the project

---
+++ Calc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calc
+{
+    public class Program    
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("{0}: Roman Numeral Calculator", args[0]);
+         }
+    }
+}
---
+++ CalcSpecs/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CalcSpecs
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
